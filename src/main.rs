6d3297f05954a3a5941c5f7127f881640f81a4b6
Camilo Lozano <camilozano98@gmail.com>
2020-09-26 19:30:22 -0400

    Sequential visits and test working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,87 @@
+use rand::Rng;
+static N: usize = 10;
+
+#[derive(Debug, Clone, Copy)]
+enum Guest {
+    Primary {counter: i32, total: i32},
+    Secondary {visited: bool}
+}
+
+impl Guest {
+    fn visit(&mut self, cupcake: &mut bool) -> bool {
+        match self{
+            Guest::Primary { counter, total } => {
+                if !*cupcake {
+                    *cupcake = true;
+                    *counter += 1;
+                }
+                *counter == *total - 1
+            }
+            Guest::Secondary { visited } => {
+                if !*visited && *cupcake{
+                    *cupcake = false;
+                    *visited = true;
+                }
+                false
+            }
+        }
+    }
+    fn gen_guest_list(count: usize) -> Vec<Guest>{
+        let mut guest_list: Vec<Guest> = vec![Guest::Secondary{visited: false}; count-1];
+        guest_list.push(Guest::Primary{counter:0,total:count as i32});
+        guest_list
+    }
+}
+
+fn start_birthday_party(number_of_guests: usize){
+
+    let mut guest_list = Guest::gen_guest_list(number_of_guests);
+    let mut cupcake = true;
+    let mut all_have_visited = false;
+
+    let mut rng = rand::thread_rng();
+    let mut x = 0;
+
+    while !all_have_visited {
+        let pick = rng.gen_range(0, number_of_guests);
+        if pick == number_of_guests-1{
+            x+=1;
+            println!("{}",x);
+        }
+        all_have_visited = guest_list[pick].visit(&mut cupcake);
+
+    }
+
+    println!("{:?}", guest_list);
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sequential_visit_all(){
+        let number_of_guests = N;
+        let mut guest_list = Guest::gen_guest_list(number_of_guests);
+        let mut cupcake = true;
+        for i in 0..number_of_guests{
+            guest_list[i].visit(&mut cupcake);
+            guest_list[number_of_guests-1].visit(&mut cupcake);
+        }
+
+        let mut num_visited = 0;
+        for g in &guest_list{
+            match &g {
+                Guest::Primary { counter: _, total: _ } => {num_visited += 1}
+                Guest::Secondary { visited } => { if *visited { num_visited +=1 } else {}}
+            }
+        }
+        assert_eq!(num_visited, number_of_guests as i32);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    start_birthday_party(N);
+
 }
